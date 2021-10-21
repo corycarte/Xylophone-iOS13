a2e2b5c6a16d2a472da634fd117db3ddbcb6ded2
Cory Carte <7457451+corycarte@users.noreply.github.com>
2021-10-21 16:11:03 -0400

    Completed basic instruction in Section 7

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -3,19 +3,29 @@
 //  Xylophone
 //
 //  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
+//  Copyright © 2019 The App Brewery. All ri/Users/carte/projects/ios_bootcamp/07 Xylophone/Xylophone/ViewController.swiftghts reserved.
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(soundName: sender.currentTitle!)
+    }
     
-
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
+
 
